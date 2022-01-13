4b6f597d971816ca2dcdfe3dcfc41e8c3716abe4
therajrajput <96807572+therajrajput@users.noreply.github.com>
2022-01-14 05:05:02 +0530

    Create find perfectnumber.py

---
+++ find perfectnumber.py
@@ -0,0 +1,12 @@
+#pthone pogramto chekt any perft number or not:
+num=int(input("enter any number"))   
+sum=0
+i=1
+while i<num:
+ if num%i==0:
+    sum=sum+i
+ i=i+1
+if num==sum:
+    print(num,"is a pefect number")
+else:
+   print(num,"is not perfet number")
