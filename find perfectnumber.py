d66f7427ec43cbee6bc1daff90da5aa0f834b6fc
Ram Kabir <55015939+ram-bit@users.noreply.github.com>
2022-01-14 09:41:09 +0530

    Update find perfectnumber.py

--- find perfectnumber.py
+++ find perfectnumber.py
@@ -1,12 +1,16 @@
 #pthone program cheak any perfet number or not:
 num=int(input("enter any number"))   
-sum=0
-i=1
-while i<num:
- if num%i==0:
-    sum=sum+i
- i=i+1
-if num==sum:
-    print(num,"is a pefect number")
-else:
-   print(num,"is not perfet number")
+def check_perfect(num):
+    sum=0
+    i=1
+    while i<num:
+        if num%i==0:
+            sum=sum+i
+        i=i+1
+    if num==sum:
+        print(num,"is a pefect number")
+    else:
+        print(num,"is not perfet number")
+    pass
+    pass
+check_perfect(num)
