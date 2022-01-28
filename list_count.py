a78cf069635fcd414dd1ef4d24fbf50cd91d273d
therajrajput <96807572+therajrajput@users.noreply.github.com>
2022-01-28 10:36:55 +0530

    Create list_count.py

---
+++ list_count.py
@@ -0,0 +1,8 @@
+a = [1,2,3,4,5,1,3,4,1,2,1,2,8,0,5,6,2,3,4,5,6,7,8,9,7,6,4,5,2,2,4,6,1,4]
+def raj(a):
+    n=sorted(set(a))
+    itre_count (n)
+def itre_count(b):
+    for i in b:
+        print(a.count(i),"-"+str(i))
+raj(a)        
